d175cea76661102166b47d03361d0aecdd5fe96e
SeboTimes <103991309+SeboTimes@users.noreply.github.com>
2024-05-05 14:10:05 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -137,7 +137,7 @@ async def play(ctx: commands.Context, url: str):
             return
 
         while len(_queue) > 0:
-            filename = f"Sounds/{genHash(url)}.mp3"
+            filename = f"Sounds/{genHash(_queue[0])}.mp3"
 
             if voice.is_connected():
                 if not exists(filename):
