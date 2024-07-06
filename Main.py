8cea137afd3239580294bcdbfdd9b5a150883b82
SeboTimes <sebochrisczemp@gmail.com>
2024-07-06 13:40:50 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -122,7 +122,6 @@ async def skip(ctx: commands.Context):
 
 @bot.slash_command(guild_ids=[guild.id for guild in bot.guilds])
 async def play(ctx: commands.Context, url: str):
-    url = url.split("?")[0]
     _queue.append(url)
 
     msg = await response(ctx, f"Loading...")
@@ -138,7 +137,7 @@ async def play(ctx: commands.Context, url: str):
         return
 
     while len(_queue) > 0:
-        filename = f"Sounds/{genHash(_queue[0])}.mp3"
+        filename = f"Sounds/{genHash(fetchYtData(_queue[0]))}.mp3"
 
         if voice.is_connected():
             if not exists(filename):
